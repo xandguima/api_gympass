6c5e2bee4c3435d4df633bc3457c93beb439419b
Alexandre <devxandeapp@gmail.com>
2025-02-16 22:52:44 -0300

    Add: development of the method on InMemoryCheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -41,9 +41,25 @@ export class InMemoryCheckInsRepository implements CheckInRepository {
     return this.items.filter((checkIn) => checkIn.user_id === userId).length
   }
 
+  async findById(id: string): Promise<CheckIn | null> {
+    const checkIn = this.items.find((item) => item.id === id)
+    if (!checkIn) {
+      return null
+    }
+    return checkIn
+  }
+
   async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
     return this.items
       .filter((checkIn) => checkIn.user_id === userId)
       .slice((page - 1) * 20, page * 20)
+  }
+
+  async save(checkIn: CheckIn): Promise<CheckIn> {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
+    return checkIn
   }
 }
