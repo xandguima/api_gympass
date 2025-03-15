88aef72e3ea2f674a477d769ab04bd15a03b76f5
Alexandre <devxandeapp@gmail.com>
2025-03-15 01:24:07 -0300

    Add: users controllers test

---
+++ src/http/controllers/users/test/perfile.spec.ts
@@ -0,0 +1,26 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { createAndAuthenticateUser } from '@/services/utils/test/create-and-authenticate-user'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to get user profile', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const authResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+    console.log(typeof authResponse.body.user.created_at)
+    expect(authResponse.statusCode).toEqual(200)
+    expect(authResponse.body.user).toEqual(
+      expect.objectContaining({ email: 'johndoe@example.com' }),
+    )
+  })
+})
---
+++ src/http/controllers/users/test/register.spec.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+
+describe('Register user (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
