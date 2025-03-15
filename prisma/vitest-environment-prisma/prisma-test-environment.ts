2ea9ce6398967b10deac18b45c70bd8fe728c963
Alexandre <devxandeapp@gmail.com>
2025-03-15 01:16:32 -0300

    Fix: environment test Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,36 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest/environments'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    console.log('Setting up Prisma environment...')
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+    process.env.DATABASE_URL = databaseUrl
+    execSync('npx prisma migrate deploy')
     return {
-      teardown() {
-        console.log('TearDown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
